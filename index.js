2b6d584588024b4e07baa6f0e027972e2939e3b9
Guilherme <borimdev@gmail.com.com>
2023-05-10 11:55:35 -0300

    finalizado

---
+++ index.js
@@ -0,0 +1,24 @@
+function calcular(event) {
+    event.preventDefault();
+
+    let alcoolInput = document.getElementById('alcool').value;
+    let gasolinaInput = document.getElementById('gasolina').value;
+    let contentResult = document.getElementById('content-result');
+    let textResult = document.getElementById('text-result');
+
+    let gasolinaSpan = document.getElementById('gasolina-result');
+    let alcoolSpan = document.getElementById('alcool-result');
+
+    let calculo = (alcoolInput / gasolinaInput);
+
+    if (calculo < 0.7) {
+        textResult.innerHTML = 'Compensa usar Álcool';
+    } else {
+        textResult.innerHTML = 'Compensa usar Gasolina';
+    }
+
+    alcoolSpan.innerHTML = 'Álcool R$ ' + alcoolInput;
+    gasolinaSpan.innerHTML = 'Gasolina R$ ' + gasolinaInput;
+
+    contentResult.classList.remove('hide');
+}
